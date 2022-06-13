22406342f49180939363dade20701ec57e4e4371
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-06-13 21:31:24 +0200

    completato primo esercizio

--- Palindromo/js/script.js
+++ Palindromo/js/script.js
@@ -2,35 +2,54 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 
-// chiedo una parola
-let userWord = 'ciao';
 
-// invoco la funzione
 
-let reverseWord = palindromWord(userWord);
+// 1. chiedere all'utente una parola
+let askWord = prompt('dimmi una parola')
+// console.log("parola chiesta all'utente", askWord);
 
-// creo la funzione
-function palindromWord(test){
+ 
+// creo una variabile booleana
+let validation = false;
+
+// 2 creo una funzione
+function palindromWord(checkWord) {
+
+   // 2.2 variabile per la concatenazione delle lettere
+    let reverseWord = "";
     
-    let reverseWord = '';
+    // 2.1 ciclo for per analizzare le parole e scomporle
+    for(i = askWord.length - 1; i >=0; i--){
 
-    let validation = false; 
-
-
-    for(let i = test.length; i >= 0; i-- ){
-
-        reverseWord = reverseWord + test[i];
-
-       
-        
+        //2.3 creo una variabile per salvare la parola al contrario
+        reverseWord = reverseWord += askWord[i];
+        // console.log('creo una variabile per la parola al contrario', reverseWord)
     }
-
+    // l'output è
     return reverseWord
-    
-    
+
 }
 
-console.log(reverseWord)
+
+// 3 invoco la funzione, tra le parentesi richiamo l'argomento che necessita per funzionare
+const flippedWord = palindromWord(askWord)
+// console.log('invoco la funzione', palindromWord)
+
+
+// 4 creo una condizione
+if(askWord == flippedWord){ 
+
+   validation = true;
+
+   console.log(validation)
+   
+}
+
+
+
+
+
+
 
 
 
