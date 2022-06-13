df2bfb61c96b5eb314c35e8dc361a7011df535fd
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-06-13 19:00:51 +0200

    eliminato refuso

--- Palindromo/js/script.js
+++ Palindromo/js/script.js
@@ -3,27 +3,11 @@
 
 
 
-// function sumGenerator(numA, numB) {
-//     const sum = numA + numB;
-//     return sum;
-// }
-
-// let result = sumGenerator(10, 10);
-// console.log(result);
-
-
-// console.log("la somma di 6 + 6: " + sumGenerator(6, 6))
-
-
-
-
-
-
 // chiedere all'utente una parola
 let userWordChoice = 'ciao';
 
 // invoco la variabile
-// palindromWord (userWordChoice);
+palindromWord (userWordChoice);
 
 // variabile flag
 let validation = false;
