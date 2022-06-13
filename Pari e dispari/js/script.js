d162a3949ff3a57f5005039371e7329c84e89031
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-06-13 23:01:37 +0200

    esercizio  terminato

---
+++ Pari e dispari/js/script.js
@@ -0,0 +1,62 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// Utente sceglie pari o dispari
+let userChoice = prompt('Scegli pari o dispari');
+
+// numero scelto dall'utente
+let userNumber = parseInt (prompt('scegli un numero tra 1 e 5'));
+// console.log("Questo è il numero dell'utente", userNumber)
+
+// evoco la funzione del pc e la salvo in una variabile
+let pcNumber = getRndInteger(1, 5);
+// console.log('Questo è il numero del pc', pcNumber)
+
+// creo una variabile per la somma dei numeri
+let sum = userNumber + pcNumber;
+console.log("questa è la somma tra user e pc", sum)
+
+// evoco la funzione numberEvenOrOdd 
+evenOrOdd = numberEvenOrOdd (sum);
+// console.log(evenOrOdd)
+
+// stabilisco chi ha vinto
+
+let userMessage;
+
+if ( userChoice == 'pari' && sum % 2 == 0 ){
+    userMessage = alert('Hai vinto');
+}else if (userChoice == 'dispari' && sum % 2 !== 0 ){
+    userMessage = alert ('Hai vinto');
+} else (
+    userMessage = alert ('Hai perso')
+)
+
+
+
+// FUNCTION
+// creo una funzione che generi un numero random per il computer 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// creo una funzione che definisca se un numero è pari o dispari
+function numberEvenOrOdd (numberToCheck){
+
+    let evenOrOdd;
+
+    if (sum % 2 == 0){
+
+        evenOrOdd = 'even';
+    } else {
+        evenOrOdd = 'odd';
+    }
+
+    return evenOrOdd
+}
+
+
